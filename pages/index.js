f2fb0e01e6ec1317fafcd2ea652f7eaff073c451
David Sinclair <mail@davidsinclair.io>
2017-11-04 20:31:49 -0700

    index and antd up

--- pages/index.js
+++ pages/index.js
@@ -1,30 +1,11 @@
 import React from 'react';
-import PropTypes from 'prop-types';
+import inject from '../lib/inject';
 import Page from '../components/Page';
 
-const Para = ({ text }) => (
-  <div>
-    <style jsx>{`
-      p {
-        color: blue;
-      }
-    `}</style>
-    <p>{text}</p>
-  </div>
-);
-
-Para.propTypes = {
-  text: PropTypes.string.isRequired,
-};
-
-export default () => (
+const Player = () => (
   <Page>
-    <style jsx>{`
-      div {
-        color: green;
-      }
-    `}</style>
-    Hello world
-    <Para text="hiii" />
+    Hello there
   </Page>
 );
+
+export default inject(Player);

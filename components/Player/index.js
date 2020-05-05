6755c0ca4ffcb2ca0e50d44da9d15b2728b277b3
David Sinclair <david@sinclair.tech>
2020-05-05 16:51:26 -0700

    make player use hooks

--- components/Player/index.js
+++ components/Player/index.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import { Dimensions, View } from 'react-native';
 import { findDOMNode } from 'react-dom';
 import PropTypes from 'prop-types';
@@ -19,295 +19,261 @@ const prepareFile = throttle((callback) => callback(), 10000, {
   leading: true,
 });
 
-class Player extends React.Component {
-  static getDerivedStateFromProps(nextProps, previousState) {
-    const oldPath = get(previousState, 'path');
-    const path = get(nextProps, 'store.player.file.path');
+const Player = ({ dispatch, store }) => {
+  const [played, playedSet] = useState(0);
+  const [playedSeconds, playedSecondsSet] = useState(0);
+  const [isFullscreen, isFullscreenSet] = useState(false);
+  const playerRef = useRef(null);
 
-    if (oldPath !== path) {
-      return {
-        played: 0,
-        playedSeconds: 0,
-        path,
-      };
-    }
+  const path = get(store, 'player.file.path');
 
-    return null;
-  }
+  useEffect(() => {
+    playedSet(0);
+    playedSecondsSet(0);
+  }, [path]);
 
-  constructor(props) {
-    super(props);
-    this.state = {
-      path: undefined,
-      played: 0,
-      playedSeconds: 0,
-      isFullscreen: false,
+  useEffect(() => {
+    const setIsFullscreen = () => isFullscreenSet(screenfull.isFullscreen);
+    screenfull.on('change', () => setIsFullscreen());
+
+    return () => {
+      screenfull.off('change', () => setIsFullscreen());
     };
-  }
+  });
 
-  componentDidMount() {
-    screenfull.on('change', () => this.setIsFullscreen());
-  }
+  const dimensions = Dimensions.get('window');
+  const styles = getStyles(dimensions);
+  const Divider = () => <Text style={styles.divider}> - </Text>;
+  const { files, player, playlists, lists } = store;
+  const {
+    source,
+    volume,
+    playing,
+    loading,
+    muted,
+    random,
+    file = {},
+    loop,
+  } = player;
+  const { url, album, artist, name, type, category } = file;
+  const config = {
+    css: styles.player,
+    loop,
+    muted,
+    width: type === 'video' ? 124 : 0,
+    height: type === 'video' ? 70 : 0,
+    progressInterval: 1000,
+    playsinline: true,
+    controls: isFullscreen,
+    volume,
+    playing,
+    url,
+    ref: playerRef,
+    onDuration: () => playerRef.current.seekTo(played),
+    onProgress: ({ played, playedSeconds }) => {
+      playedSet(played);
+      playedSecondsSet(playedSeconds);
 
-  componentDidUpdate(previousProps) {
-    const { store, dispatch } = this.props;
-    const { path } = this.state;
-    const {
-      player: { source, playing },
-      cloud: { isConnected },
-    } = store;
-    const isConnectedPrevious = get(previousProps, 'store.cloud.isConnected');
-
-    if (isConnected !== isConnectedPrevious && playing && source && path) {
-      filesGetUrl({ dispatch, store, source, path, shouldPlay: true });
-    }
-  }
-
-  componentWillUnmount() {
-    screenfull.off('change', () => this.setIsFullscreen());
-  }
-
-  setIsFullscreen() {
-    this.setState({ isFullscreen: screenfull.isFullscreen });
-  }
-
-  render() {
-    const dimensions = Dimensions.get('window');
-    const styles = getStyles(dimensions);
-    const Divider = () => <Text style={styles.divider}> - </Text>;
-    const { played, playedSeconds, path, isFullscreen } = this.state;
-    const { dispatch, store } = this.props;
-    const { files, player, playlists, lists } = store;
-    const {
-      source,
-      volume,
-      playing,
-      loading,
-      muted,
-      random,
-      file = {},
-      loop,
-    } = player;
-    const { url, album, artist, name, type, category } = file;
-    const config = {
-      css: styles.player,
-      loop,
-      muted,
-      width: type === 'video' ? 124 : 0,
-      height: type === 'video' ? 70 : 0,
-      progressInterval: 1000,
-      playsinline: true,
-      controls: isFullscreen,
-      volume,
-      playing,
-      url,
-      ref: (ref) => {
-        this.player = ref;
-      },
-      onDuration: () => this.player.seekTo(played),
-      onProgress: ({ played, playedSeconds }) => {
-        this.setState({ played, playedSeconds });
-
-        // Get next link once 90% of media is over
-        if (playing && played > 0.9) {
-          prepareFile(() =>
-            filesGetUrl({
-              dispatch,
-              store,
-              ...getFileInDirection({ player, lists, files, playlists }),
-              source,
-            }),
-          );
-        }
-      },
-      onEnded: () =>
-        filesGetUrl({
-          dispatch,
-          store,
-          ...getFileInDirection({
-            player,
-            lists,
-            files,
-            playlists,
-            direction: random ? 'random' : 'next',
+      // Get next link once 90% of media is over
+      if (playing && played > 0.9) {
+        prepareFile(() =>
+          filesGetUrl({
+            dispatch,
+            store,
+            ...getFileInDirection({ player, lists, files, playlists }),
+            source,
           }),
-          source,
-          shouldPlay: true,
+        );
+      }
+    },
+    onEnded: () =>
+      filesGetUrl({
+        dispatch,
+        store,
+        ...getFileInDirection({
+          player,
+          lists,
+          files,
+          playlists,
+          direction: random ? 'random' : 'next',
         }),
-      onClick: () => screenfull.request(findDOMNode(this.player)),
-    };
-    const LoopButton = (props) => (
-      <Switch
-        checkedIcon="loop"
-        unCheckedIcon="loop"
-        isOn={loop}
-        onValueChange={() =>
-          dispatch({ type: 'player-update', payload: ['loop', !loop] })
-        }
-        {...props}
-      />
-    );
-    const ShuffleButton = (props) => (
-      <Switch
-        checkedIcon="shuffle"
-        unCheckedIcon="shuffle"
-        isOn={random}
-        onValueChange={() =>
-          dispatch({ type: 'player-update', payload: ['random', !random] })
-        }
-        {...props}
-      />
-    );
+        source,
+        shouldPlay: true,
+      }),
+    // eslint-disable-next-line react/no-find-dom-node
+    onClick: () => screenfull.request(findDOMNode(playerRef.current)),
+  };
+  const LoopButton = (props) => (
+    <Switch
+      checkedIcon="loop"
+      unCheckedIcon="loop"
+      isOn={loop}
+      onValueChange={() =>
+        dispatch({ type: 'player-update', payload: ['loop', !loop] })
+      }
+      {...props}
+    />
+  );
+  const ShuffleButton = (props) => (
+    <Switch
+      checkedIcon="shuffle"
+      unCheckedIcon="shuffle"
+      isOn={random}
+      onValueChange={() =>
+        dispatch({ type: 'player-update', payload: ['random', !random] })
+      }
+      {...props}
+    />
+  );
 
-    return (
-      <View style={styles.root}>
-        <ReactPlayer {...config} />
-        <View style={styles.main}>
-          <View style={styles.info}>
-            {path ? (
-              <>
-                <Text fontWeight="bold" style={styles.name}>
-                  {name}
+  return (
+    <View style={styles.root}>
+      <ReactPlayer {...config} />
+      <View style={styles.main}>
+        <View style={styles.info}>
+          {path ? (
+            <>
+              <Text fontWeight="bold" style={styles.name}>
+                {name}
+              </Text>
+              {category && (
+                <Text>
+                  <Divider />
+                  {category}
                 </Text>
-                {category && (
-                  <Text>
-                    <Divider />
-                    {category}
-                  </Text>
-                )}
-                {artist && (
-                  <Text>
-                    <Divider />
-                    {artist}
-                  </Text>
-                )}
-                {album && (
-                  <Text>
-                    <Divider />
-                    {album}
-                  </Text>
-                )}
-              </>
-            ) : (
-              <Text>Add credentials and play some media</Text>
-            )}
-          </View>
-          <View style={styles.directions}>
-            <LoopButton style={styles.loopDirections} />
-            <View style={styles.buttons}>
-              <Button
-                size="large"
-                shape="circle"
-                icon="fast-backward"
-                onPress={() =>
+              )}
+              {artist && (
+                <Text>
+                  <Divider />
+                  {artist}
+                </Text>
+              )}
+              {album && (
+                <Text>
+                  <Divider />
+                  {album}
+                </Text>
+              )}
+            </>
+          ) : (
+            <Text>Add credentials and play some media</Text>
+          )}
+        </View>
+        <View style={styles.directions}>
+          <LoopButton style={styles.loopDirections} />
+          <View style={styles.buttons}>
+            <Button
+              size="large"
+              shape="circle"
+              icon="fast-backward"
+              onPress={() =>
+                filesGetUrl({
+                  dispatch,
+                  store,
+                  ...getFileInDirection({
+                    player,
+                    lists,
+                    files,
+                    playlists,
+                    direction: random ? 'random' : 'previous',
+                  }),
+                  source,
+                  shouldPlay: true,
+                })
+              }
+            />
+            <Button
+              size="large"
+              shape="circle"
+              icon={playing ? 'pause' : 'play'}
+              isLoading={loading}
+              onPress={() => {
+                if (!playing) {
                   filesGetUrl({
                     dispatch,
                     store,
-                    ...getFileInDirection({
-                      player,
-                      lists,
-                      files,
-                      playlists,
-                      direction: random ? 'random' : 'previous',
-                    }),
                     source,
+                    path,
                     shouldPlay: true,
-                  })
-                }
-              />
-              <Button
-                size="large"
-                shape="circle"
-                icon={playing ? 'pause' : 'play'}
-                isLoading={loading}
-                onPress={() => {
-                  if (!playing) {
-                    filesGetUrl({
-                      dispatch,
-                      store,
-                      source,
-                      path,
-                      shouldPlay: true,
-                    });
-                  }
-
-                  dispatch({
-                    type: 'player-update',
-                    payload: ['playing', !playing],
                   });
-                }}
-              />
-              <Button
-                size="large"
-                shape="circle"
-                icon="fast-forward"
-                onPress={() =>
-                  filesGetUrl({
-                    dispatch,
-                    store,
-                    ...getFileInDirection({
-                      player,
-                      lists,
-                      files,
-                      playlists,
-                      direction: random ? 'random' : 'next',
-                    }),
-                    source,
-                    shouldPlay: true,
-                  })
                 }
-              />
-            </View>
-            <ShuffleButton style={styles.shuffleDirections} />
-          </View>
-          <View style={styles.sound}>
-            <View style={styles.switches}>
-              <ShuffleButton style={styles.shuffleSound} />
-              <LoopButton style={styles.loopSound} />
-              <Switch
-                color={colors.c}
-                checkedIcon="volume"
-                unCheckedIcon="mute"
-                isOn={!muted}
-                onValueChange={() =>
-                  dispatch({
-                    type: 'player-update',
-                    payload: ['muted', !muted],
-                  })
-                }
-              />
-            </View>
-            <Slider
-              color={colors.c}
-              value={volume}
-              min={0}
-              max={1}
-              tip={`Volume: ${Math.round(volume * 100)}%`}
-              onChange={(volume) =>
+
                 dispatch({
                   type: 'player-update',
-                  payload: ['volume', volume],
+                  payload: ['playing', !playing],
+                });
+              }}
+            />
+            <Button
+              size="large"
+              shape="circle"
+              icon="fast-forward"
+              onPress={() =>
+                filesGetUrl({
+                  dispatch,
+                  store,
+                  ...getFileInDirection({
+                    player,
+                    lists,
+                    files,
+                    playlists,
+                    direction: random ? 'random' : 'next',
+                  }),
+                  source,
+                  shouldPlay: true,
+                })
+              }
+            />
+          </View>
+          <ShuffleButton style={styles.shuffleDirections} />
+        </View>
+        <View style={styles.sound}>
+          <View style={styles.switches}>
+            <ShuffleButton style={styles.shuffleSound} />
+            <LoopButton style={styles.loopSound} />
+            <Switch
+              color={colors.c}
+              checkedIcon="volume"
+              unCheckedIcon="mute"
+              isOn={!muted}
+              onValueChange={() =>
+                dispatch({
+                  type: 'player-update',
+                  payload: ['muted', !muted],
                 })
               }
             />
           </View>
           <Slider
-            style={styles.progress}
-            value={played}
+            color={colors.c}
+            value={volume}
             min={0}
             max={1}
-            step={0.01}
-            tip={moment(
-              // eslint-disable-next-line no-underscore-dangle
-              moment.duration(playedSeconds, 'seconds')._data,
-            ).format('mm:ss')}
-            onChange={(progress) => this.player.seekTo(progress)}
+            tip={`Volume: ${Math.round(volume * 100)}%`}
+            onChange={(volume) =>
+              dispatch({
+                type: 'player-update',
+                payload: ['volume', volume],
+              })
+            }
           />
         </View>
+        <Slider
+          style={styles.progress}
+          value={played}
+          min={0}
+          max={1}
+          step={0.01}
+          tip={moment(
+            // eslint-disable-next-line no-underscore-dangle
+            moment.duration(playedSeconds, 'seconds')._data,
+          ).format('mm:ss')}
+          onChange={(progress) => playerRef.current.seekTo(progress)}
+        />
       </View>
-    );
-  }
-}
+    </View>
+  );
+};
 
 Player.propTypes = {
   store: PropTypes.object.isRequired,

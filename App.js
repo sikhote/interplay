7bdf2027830a47adf16879eefbf5533065cbf269
David Sinclair <david@sinclair.tech>
2020-05-22 09:31:17 -0700

    progress check

---
+++ App.js
@@ -0,0 +1,53 @@
+import { Ionicons } from '@expo/vector-icons';
+import { SplashScreen } from 'expo';
+import * as Font from 'expo-font';
+import React from 'react';
+import { StyleSheet, View, Text } from 'react-native';
+
+export default function App(props) {
+  const [isLoadingComplete, setLoadingComplete] = React.useState(false);
+
+  // Load any resources or data that we need prior to rendering the app
+  React.useEffect(() => {
+    async function loadResourcesAndDataAsync() {
+      try {
+        SplashScreen.preventAutoHide();
+
+        // Load fonts
+        await Font.loadAsync({
+          ...Ionicons.font,
+          'space-mono': require('./public/fonts/SpaceMono-Regular.ttf'),
+        });
+      } catch (error) {
+        // We might want to provide this error information to an error reporting service
+        console.warn(error);
+      } finally {
+        setLoadingComplete(true);
+        SplashScreen.hide();
+      }
+    }
+
+    loadResourcesAndDataAsync();
+  }, []);
+
+  if (!isLoadingComplete && !props.skipLoadingScreen) {
+    return null;
+  }
+
+  return (
+    <View style={styles.container}>
+      <Text style={styles.tabBarInfoText}>This is!!!!</Text>
+      <Text style={styles.tabBarInfoText}>This is!!!!</Text>
+      <Text style={styles.tabBarInfoText}>This is!!!!</Text>
+      <Text style={styles.tabBarInfoText}>This is!!!!</Text>
+      <Text style={styles.tabBarInfoText}>This is11111111111</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+  },
+});
--- babel.config.js
+++ babel.config.js
@@ -1,4 +1,7 @@
-module.exports = {
-  presets: ['next/babel'],
-  plugins: [['react-native-web', { commonjs: true }]],
+module.exports = (api) => {
+  api.cache(true);
+  return {
+    presets: ['babel-preset-expo', 'next/babel'],
+    // Plugins: [['react-native-web', { commonjs: true }]],
+  };
 };
